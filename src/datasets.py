9a23f256283256c887fd8ffb43356cffe29c3480
pchmiel <piotr.chmiel@hotmail.com>
2016-12-12 19:09:45 +0100

    Add Activity Recognition from Single Chest-Mounted Accelerometer Data Set

--- src/datasets.py
+++ src/datasets.py
@@ -1,5 +1,9 @@
 import csv
 import os
+import zipfile
+import random
+import urllib.request
+import numpy as np
 from src.settings import DATASET_DIR
 from enum import Enum, unique
 from sklearn.model_selection import train_test_split
@@ -15,14 +19,18 @@ class LearningSetFactory(object):
     @unique
     class DataSource(Enum):
         breast_cancer = 1
+        activity_reccognition = 2
 
     def get_train_test_data(self, data_source):
         if data_source == LearningSetFactory.DataSource.breast_cancer:
             data, target = self.get_breast_cancer()
+        elif data_source == LearningSetFactory.DataSource.activity_reccognition:
+            return self.get_activity_reccognition()
         else:
             raise Exception("Invalid data source. Check DataSource enum class.")
 
-        return train_test_split(data, target, test_size=self.test_size)
+        X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=self.test_size)
+        return X_train, y_train, [len(y_train)], X_test, y_test, [len(y_test)]
 
     def get_breast_cancer(self):
         data = []
@@ -41,3 +49,43 @@ class LearningSetFactory(object):
                 data.append(row)
 
         return DictVectorizer(sparse=False).fit_transform(data, target), target
+
+    def get_activity_reccognition(self):
+        url = "https://archive.ics.uci.edu/ml/machine-learning-databases/" \
+              "00287/Activity%20Recognition%20from%20Single%20Chest-Mounted%20Accelerometer.zip"
+
+        target_path = os.path.join(DATASET_DIR, "ARFSCMA.zip")
+
+        if not os.path.exists(target_path):
+            urllib.request.urlretrieve(url, target_path)
+
+        with zipfile.ZipFile(target_path, "r") as zip_ref:
+            zip_ref.extractall(os.path.join(DATASET_DIR))
+
+        unpack_dir = os.path.join(DATASET_DIR, "Activity Recognition from Single Chest-Mounted Accelerometer")
+        csv_files = [os.path.join(unpack_dir, file) for file in os.listdir(unpack_dir) if file.endswith(".csv")]
+        random.shuffle(csv_files)
+
+        split_point = int(0.2 * len(csv_files))
+        test = slice(0, split_point)
+        train = slice(split_point, len(csv_files))
+
+        return self.get_data_from_csv_without_header(csv_files[test]) + \
+               self.get_data_from_csv_without_header(csv_files[train])
+
+    def get_data_from_csv_without_header(self, filenames):
+        data = []
+        labels = []
+        sequence_lengths = []
+        for file in filenames:
+            counter = 0
+            with open(file, 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    *raw_data, label = row
+                    data.append(raw_data)
+                    labels.append(label)
+                    counter += 1
+            sequence_lengths.append(counter)
+
+        return np.asarray(data, np.float64), np.asarray(labels), sequence_lengths
--- src/learn.py
+++ src/learn.py
@@ -3,12 +3,12 @@ from seqlearn.hmm import MultinomialHMM
 from sklearn.metrics import accuracy_score
 
 
-def train_and_test_markov(X_train, X_test, y_train, y_test):
+def train_and_test_markov(X_train, y_train, sequence_length_train, X_test, y_test, sequence_length_test):
     clf = MultinomialHMM()
-    clf.fit(X_train, y_train, len(y_train))
+    clf.fit(X_train, y_train, sequence_length_train)
     print("Training {}".format(clf))
     print(len(y_test))
-    y_pred = clf.predict(X_test)
+    y_pred = clf.predict(X_test, sequence_length_test)
     print("Accuracy: {0:.2f}".format(100 * accuracy_score(y_pred, y_test)))
 
 
@@ -18,8 +18,9 @@ def train_and_test_bayes():
 
 def main():
     learning_factory = LearningSetFactory()
-    X_train, X_test, y_train, y_test = learning_factory.get_train_test_data(LearningSetFactory.DataSource.breast_cancer)
-    train_and_test_markov(X_train, X_test, y_train, y_test)
+    train_and_test_markov(*learning_factory.get_train_test_data(LearningSetFactory.DataSource.breast_cancer))
+    print(len(learning_factory.get_train_test_data(LearningSetFactory.DataSource.activity_reccognition)))
+    train_and_test_markov(*learning_factory.get_train_test_data(LearningSetFactory.DataSource.activity_reccognition))
 
 if __name__ == "__main__":
     main()
