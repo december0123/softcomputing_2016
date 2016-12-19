1e20c8a35a42d19ea7a34f1169fb84a59e90fc9c
jagub2 <jagub2@users.noreply.github.com>
2016-12-19 22:04:32 +0100

    add Bayes classificator

--- src/learn.py
+++ src/learn.py
@@ -1,12 +1,8 @@
 from src.datasets import LearningSetFactory
 from seqlearn.hmm import MultinomialHMM
 from sklearn.metrics import accuracy_score
-from pomegranate import BayesianNetwork
-from sklearn.feature_extraction import DictVectorizer
-from copy import deepcopy
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import preprocessing
+
+from src.MyBayes import MyBayes
 
 def train_and_test_markov(X_train, y_train, sequence_length_train, X_test, y_test,
                           sequence_length_test, *args, **kwargs):
@@ -21,27 +17,16 @@ def train_and_test_markov(X_train, y_train, sequence_length_train, X_test, y_tes
 def train_and_test_bayes(X_train, y_train, sequence_length_train, X_test, y_test,
                           sequence_length_test, feature_names, *args, **kwargs):
     print("Bayes network training")
-    le = preprocessing.LabelEncoder()
-    formatted_labels = le.fit_transform(y_train)
-    formatted_labels = formatted_labels.reshape(formatted_labels.shape[0], 1)
-    print("Classes", le.classes_)
-    X = np.concatenate((X_train, formatted_labels), axis=1)
-    print("After merge ", X.shape)
-    state_names = deepcopy(feature_names)
-    state_names.extend("Cancer_type")
-    model = BayesianNetwork.from_samples(X, algorithm='chow-liu', state_names=state_names)
+    clf = MyBayes(feature_names)
+    clf.fit(X_train, y_train, sequence_length_train)
+    print("Training {}".format(clf))
+    print(len(y_test))
+    y_pred = clf.predict(X_test, sequence_length_test)
+    # print("Accuracy: {0:.2f}".format(100 * accuracy_score(y_pred, y_test)))
 
-    plt.figure(figsize=(16, 8))
-    model.plot()
-    plt.show()
-
-    formatted_test_labels = le.fit_transform(y_test)
-    formatted_test_labels = formatted_test_labels.reshape(formatted_test_labels.shape[0], 1)
-    Test = np.concatenate((X_train, formatted_test_labels), axis=1)
-
-
-    print(model.predict_proba({ state_names[0]: 0}))
-    raise NotImplementedError
+    # plt.figure(figsize=(16, 8))
+    # clf.model.plot()
+    # plt.show()
 
 
 def main():
