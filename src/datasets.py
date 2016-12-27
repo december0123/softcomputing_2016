aed0412174d2d7b7866102363f8edf1e8b38ce8e
Łukasz Matysiak <december0123@gmail.com>
2016-12-27 02:16:45 +0100

    Implement creating sequenced data for breast cancer

--- src/datasets.py
+++ src/datasets.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import csv
 import os
 import zipfile
@@ -8,18 +9,20 @@ from src.settings import DATASET_DIR
 from enum import Enum, unique
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction import DictVectorizer
+from src.markov_chain_transition_matrix_generator import MarkovChainTransitionMatrixGenerator
 
 
 class LearningSetFactory(object):
-
-    def __init__(self, test_size=0.2):
+    def __init__(self, test_size=0.2, sequence_length=2):
         print(test_size)
         self.test_size = test_size
+        self.sequence_length = sequence_length
 
     @unique
     class DataSource(Enum):
         breast_cancer = 1
         activity_recognition = 2
+        sequenced_breast_cancer = 3
 
     def get_train_test_data(self, data_source):
         print("Data source: {}" .format(data_source))
@@ -27,11 +30,122 @@ class LearningSetFactory(object):
             data, target, feature_names = self.get_breast_cancer()
         elif data_source == LearningSetFactory.DataSource.activity_recognition:
             return self.get_activity_recognition()
+        elif data_source == LearningSetFactory.DataSource.sequenced_breast_cancer:
+            return self.get_sequenced_breast_cancer()
         else:
             raise Exception("Invalid data source. Check DataSource enum class.")
 
         X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=self.test_size)
         return X_train, y_train, [len(y_train)], X_test, y_test, [len(y_test)], feature_names
+
+    def get_sequenced_breast_cancer(self):
+        # proof of concept
+        grouped_by_labels, labels, features = self.extract_breast_cancer_data_grouped_by_label()
+        sequenced_X, sequenced_Y = self.create_sequences(grouped_by_labels, labels)
+        sequenced_X, sequenced_Y = self.shuffle_sequences(sequenced_X, sequenced_Y)
+
+        vectorizer = DictVectorizer(sparse=False)
+        sequenced_X = vectorizer.fit_transform(sequenced_X)
+
+        X_test, X_train, y_test, y_train = self.split_sets(sequenced_X, sequenced_Y)
+        train_seq_lengths = self.get_sequence_lengths(y_train)
+        test_seq_lengths = self.get_sequence_lengths(y_test)
+
+        return X_train, y_train, train_seq_lengths, X_test, y_test, test_seq_lengths, features
+
+    def create_sequences(self, grouped_by_labels, labels):
+        # the idea behind this is to make sequenced data out of non sequenced data:
+        # 1. create a matrix with probabilities to switch class or stay
+        # 2. sort samples by class
+        # 3. for all samples
+        # 4.    pick the next class with respect to the probabilities in the matrix
+        # 5.    pick a random sample from that class
+        # 6.    we have a sequence!
+
+        # 1.
+        gen = MarkovChainTransitionMatrixGenerator(len(grouped_by_labels))
+        # 2.
+        matrix = gen.get_transition_probability_matrix()
+        current_label_index = 0
+        sequenced_X = []
+        sequenced_Y = []
+
+        # 3.
+        # note: dicts are not ordered but chyba wyjebane
+        for label, rows in grouped_by_labels.items():
+            for row in rows:
+                sequenced_X.append(row)
+                sequenced_Y.append(label)
+                # 4.
+                # False is kinda zero, so this works
+                possible_next_labels_indices = np.nonzero(matrix[current_label_index] > 0)[0]
+                probabilities = matrix[current_label_index][possible_next_labels_indices]
+                next_label_index = np.random.choice(possible_next_labels_indices, p=probabilities)
+                next_label = labels[next_label_index]
+                # 5.
+                next_in_seq = np.random.choice(grouped_by_labels[next_label])
+                # 6.
+                sequenced_X.append(next_in_seq)
+                sequenced_Y.append(next_label)
+        return sequenced_X, sequenced_Y
+
+    # MultinomialHMM requires an array containing lengths of all sequences
+    def get_sequence_lengths(self, y_train):
+        return [self.sequence_length for _ in range(int(len(y_train) / self.sequence_length))]
+
+    def extract_breast_cancer_data_grouped_by_label(self):
+        labels = []
+        grouped = defaultdict(list)
+        bool_map = {'yes': 1, 'no': 0, 'nan': 0}
+        with open(os.path.join(DATASET_DIR, 'uci-20070111-breast-cancer.csv')) as csvfile:
+            readCSV = csv.DictReader(csvfile)
+            for row in readCSV:
+                for key in row:
+                    row[key] = row[key].strip("'")
+                row['deg-malig'] = int(row['deg-malig'])
+                row['node-caps'] = bool_map[row['node-caps']]
+                row['irradiat'] = bool_map[row['irradiat']]
+                label = row['label']
+                labels.append(label)
+                row.pop('label')
+                grouped[label].append(row)
+        features = ('age', 'menopause', 'tumor-size', 'inv-nodes', 'node-caps', 'deg-malig', 'breast', 'breast-quad',
+         'irradiat')
+        return grouped, labels, features
+
+    def get_split_point(self, sequenced_Y):
+        split_point = int(0.2 * len(sequenced_Y))
+        while split_point % self.sequence_length > 0:
+            split_point += 1
+        return split_point
+
+    def split_sets(self, sequenced_X, sequenced_Y):
+        split_point = self.get_split_point(sequenced_Y=sequenced_Y)
+        X_test = sequenced_X[:split_point]
+        X_train = sequenced_X[split_point:]
+        y_test = sequenced_Y[:split_point]
+        y_train = sequenced_Y[split_point:]
+        return X_test, X_train, y_test, y_train
+
+    def shuffle_sequences(self, sequenced_X, sequenced_Y):
+        seq_length_counter = 0
+        to_shuffle = []
+        package = []
+        for zipped in zip(sequenced_X, sequenced_Y):
+            package.append(zipped)
+            seq_length_counter += 1
+            if seq_length_counter == self.sequence_length:
+                to_shuffle.append(package)
+                seq_length_counter = 0
+                package = []
+        random.shuffle(to_shuffle)
+        sequenced_X = []
+        sequenced_Y = []
+        for list_of_sequences in to_shuffle:
+            for zipped_sequence in list_of_sequences:
+                sequenced_X.append(zipped_sequence[0])
+                sequenced_Y.append(zipped_sequence[1])
+        return sequenced_X, sequenced_Y
 
     def get_breast_cancer(self):
         data = []
@@ -89,8 +203,14 @@ class LearningSetFactory(object):
             with open(file, 'r') as csvfile:
                 reader = csv.reader(csvfile)
                 for row in reader:
+                    # if counter > 5:
+                    #     raise
                     *raw_data, label = row
                     raw_data = raw_data[skip_columns:]
+                    # print("data", data)
+                    # print("raw data", raw_data)
+                    # print("labels", labels)
+                    # print("sequence_lengths", sequence_lengths)
                     data.append(raw_data)
                     labels.append(label)
                     counter += 1
--- src/learn.py
+++ src/learn.py
@@ -7,6 +7,12 @@ from src.MyBayes import MyBayes
 def train_and_test_markov(X_train, y_train, sequence_length_train, X_test, y_test,
                           sequence_length_test, *args, **kwargs):
     clf = MultinomialHMM()
+    print("X_train")
+    print(len(X_train))
+    print("y_train")
+    print(len(y_train))
+    print("sequence_length_train")
+    print(len(sequence_length_train))
     clf.fit(X_train, y_train, sequence_length_train)
     print("Training {}".format(clf))
     print(len(y_test))
@@ -27,10 +33,10 @@ def train_and_test_bayes(X_train, y_train, sequence_length_train, X_test, y_test
 
 def main():
     learning_factory = LearningSetFactory()
-    train_and_test_bayes(*learning_factory.get_train_test_data(LearningSetFactory.DataSource.breast_cancer))
     # train_and_test_bayes(*learning_factory.get_train_test_data(LearningSetFactory.DataSource.activity_recognition))
+    # train_and_test_bayes(*learning_factory.get_train_test_data(LearningSetFactory.DataSource.breast_cancer))
     train_and_test_markov(*learning_factory.get_train_test_data(LearningSetFactory.DataSource.breast_cancer))
-    # train_and_test_markov(*learning_factory.get_train_test_data(LearningSetFactory.DataSource.activity_recognition))
+    train_and_test_markov(*learning_factory.get_train_test_data(LearningSetFactory.DataSource.sequenced_breast_cancer))
 
 if __name__ == "__main__":
     main()
